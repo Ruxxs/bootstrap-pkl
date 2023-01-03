00df39a73c140199020107f2c3744a99fbc03d47
Ruxxs <121854591+Ruxxs@users.noreply.github.com>
2023-01-03 15:13:25 +0700

    versi 1

---
+++ PKL FADLI/variabel/1.1.php
@@ -0,0 +1,37 @@
+<?php 
+
+// $nama = "FADLI" ;
+// $umur = 17 ;
+// $kelas = "11 RPL";
+// $alamat = "SBW" ;
+// echo "nama saya $nama, umur saya $umur tahun, saya kelas $kelas alamat saya $alamat";
+
+
+ $nilai = 100;
+ if ($nilai <100) {
+     echo "Lulus";
+ }else if ($nilai >80 && $nilai <=90){
+ echo "Dapat diperbaiki";
+ }else {
+    echo "tidak lulus";
+ }
+
+
+// $beli = "bang bang";
+// if ($beli == "gula") {
+//     echo "Anda membeli gula";
+// }else if ($beli == "beras") {
+// echo "beli beras berapa kilo";
+// }else if ($beli == "minyak") {
+//  echo "Anda membeli minyak"  ; 
+// }else {
+//     echo "ya udah beli bang bang aja ";
+// }
+
+
+
+
+
+
+
+?>
